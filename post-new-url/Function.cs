7497f29ddd32e11076b72742208791de75d73016
sebandres <sebandres@gmail.com>
2017-08-08 20:39:53 +1000

    bind to POCO object

--- post-new-url/Function.cs
+++ post-new-url/Function.cs
@@ -16,16 +16,14 @@ namespace urlshortener
 {
     public class PostNewUrl
     {
-        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, string name)
+        public static async Task<HttpResponseMessage> Run(ShortUrlRequest request, string name)
         {
             Assembly assembly = typeof(PostNewUrl).GetTypeInfo().Assembly;
 
             var builder = new ConfigurationBuilder()
                 .AddJsonFile(new EmbeddedFileProvider(assembly, "post_new_url"), "appsettings.json", true, false);
 
-            var configuration = builder.Build();
-
-            var request = new ShortUrlRequest { OriginalUrl = name };
+            var configuration = builder.Build();          
 
             var result = new ShortUrlResponse(
                 (shortUrl) => ShortenUrlFunctions.SaveShortUrl(() => configuration["StorageConnection"], shortUrl),
