ce03222b4ff7eccc73a9dbe04596f422b70be0fd
sebandres <sebandres@gmail.com>
2017-08-10 21:46:52 +1000

    Added get-url

---
+++ get-url/Function.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using Urlshortener.Functions;
+
+namespace urlshortener
+{
+    public class GetUrl
+    {
+        public static async Task<HttpResponseMessage> Run(string hash)
+        {
+            Assembly assembly = typeof(GetUrl).GetTypeInfo().Assembly;
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile(new EmbeddedFileProvider(assembly, "post_new_url"), "appsettings.json", true, false);
+
+            var configuration = builder.Build();
+
+            var shortUrl = await ShortenUrlFunctions
+                .RetrieveShortUrl(
+                    () => configuration["StorageConnection"],
+                    hash);
+
+            if (shortUrl == null)
+            {
+                return new HttpResponseMessage
+                {
+                    StatusCode = System.Net.HttpStatusCode.NotFound
+                };
+            }
+            else
+            {
+                var response = new HttpResponseMessage
+                {
+                    StatusCode = System.Net.HttpStatusCode.Redirect,
+                };
+                response.Headers.Location = new System.Uri(shortUrl);
+                return response;
+            }
+        }
+    }
+}
